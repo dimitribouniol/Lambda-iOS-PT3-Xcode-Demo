15bf7facb4cfcaebb594e35ae17f50bc56b5ce92
Dimitri Bouniol <dimitri.bouniol@lambdaschool.com>
2019-09-11 19:39:37 -0700

    Initial Commit

---
+++ Xcode Demo/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Xcode Demo
+//
+//  Created by Dimitri Bouniol Lambda on 9/11/19.
+//  Copyright © 2019 My really awesome company, inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Command-option-shift: choose where to open a file
+        // Command-/ : comment/uncomment a line of code
+        // Control-T : transpose characters
+        // Control-Command-E: Edit all in Scope
+        // Control-I : re-indent selection
+        // Command-[] : move indentation left/right
+        
+        var firstName = "Dimitri"
+        print("My name is \(firstName)")
+        
+        if true {
+            // test
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
