74808e8df513662abbe744134b9383426d2c5b82
Gavin Murphy <gavincmurphy@gmail.com>
2019-09-11 20:19:02 -0700

    Removed a -

--- Xcode Demo/ViewController.swift
+++ Xcode Demo/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
         // Control-I : re-indent selection
         // Command-[] : move indentation left/right
         
-        let fullName = "Pseudo-Gavin-Dimitri Bouniol"
+        let fullName = "PseudoGavin-Dimitri Bouniol"
         print("My name is \(fullName)!")
         
         // Do any additional setup after loading the view.
